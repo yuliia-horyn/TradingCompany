e4b286f3b305ff5265c3c0ed77111be9c329cf9d
yuliia-horyn <79104694+yuliia-horyn@users.noreply.github.com>
2022-10-14 02:03:46 +0300

    Update UnitTest1.cs

--- UnitTestDAL/UnitTest1.cs
+++ UnitTestDAL/UnitTest1.cs
@@ -1,6 +1,8 @@
-﻿using AutoMapper;
+using AutoMapper;
 using DAL.Complete;
+using DTO;
 using NUnit.Framework;
+using System.Configuration;
 using System.EnterpriseServices;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -9,7 +11,7 @@ namespace UnitTestDAL
 {
     [TestFixture]
     [Transaction(TransactionOption.RequiresNew),ComVisible(true)]
-    public class UnitTest1
+    public class UserDalTest:ServicedComponent
     {
         private IMapper _mapper;
         [OneTimeSetUp]
@@ -21,13 +23,62 @@ namespace UnitTestDAL
             _mapper= config.CreateMapper();
         }
         [Test]
-        public void TestMethod1()
+        public void UserIsInDB()
         {
-            
+            //Arange
             var userDal = new UserDal(_mapper);
+            //Act
             var users = userDal.GetAllUsers();
+            //Assert
             Assert.IsNotNull(users);
             Assert.Contains("Anna",users.Select(u=>u.FirstName).ToList());
+        }
+        [Test]
+        public void CreateUserTest()
+        {
+            UserDal dal = new UserDal(_mapper);
+            var result = dal.CreateUser(new UserDTO
+            {
+                FirstName = "Jane",
+                LastName = "Brown",
+                Login = "jannie",
+                Password = "9071",
+                Keyword = "moon",
+                Gender = "women",
+                Address = "871 West Brickell Street Hochelaga,QC H1W 9R5",
+                Email = "jane27@gmail.com",
+                PhoneNumber = "0967123455",
+                BankCard = "2309 8175 4650 3028"
+            });
+            Assert.IsTrue(result.UserID != 0, "returned ID>0");
+        }
+        [Test]
+        public void GetAllUsersTest()
+        {
+            UserDal dal = new UserDal(_mapper);
+            var result = dal.CreateUser(new UserDTO
+            {
+                FirstName="Jane",
+                LastName="Brown",
+                Login="jannie",
+                Password="9071",
+                Keyword="moon",
+                Gender="women",
+                Address= "871 West Brickell Street Hochelaga,QC H1W 9R5",
+                Email="jane27@gmail.com",
+                PhoneNumber="0967123455",
+                BankCard="2309 8175 4650 3028"
+            });
+            var users = dal.GetAllUsers();
+            Assert.AreEqual(1, users.Count(x => x.Keyword == "moon"));
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            if (ContextUtil.IsInTransaction)
+            {
+                ContextUtil.SetAbort();
+            }
         }
     }
 }
