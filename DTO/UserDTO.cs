5157f8861946756cda75b96a01c23ef427b430c6
yuliia-horyn <79104694+yuliia-horyn@users.noreply.github.com>
2022-11-24 02:58:13 +0200

    WF

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -8,9 +8,10 @@ namespace DTO
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Login { get; set; }
-        public string Password { get; set; }
+        public byte[] Passsword { get; set; }
+        public Guid Salt{ get; set; }
         public string Keyword { get; set; }
-        public string Gender { get; set; }
+        public bool IsFemale { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
@@ -19,7 +20,7 @@ namespace DTO
         public DateTime RowUpdateTime { get; set; }
         public override string ToString()
         {
-            return $"{UserID,-3}*\t{FirstName,-8}*\t{LastName,-8}* {Gender,-6}*\t{Address,-52}*\t{Email,-25}* {PhoneNumber,-10} * {BankCard,-20}* {RowInsertTime} * {RowUpdateTime}";
+            return $"{UserID,-5}*\t{FirstName,-8}*\t{LastName,-8}* {IsFemale,-6}*\t{Address,-52}*\t{Email,-25}* {PhoneNumber,-10} * {BankCard,-20}* {RowInsertTime} * {RowUpdateTime}";
         }
     }
 }
